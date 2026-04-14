789879a351936545d1576a55ff2e6cfeeeee3998
Anna Kolupaeva <akolupae@c1r2p6.hive.fi>
2026-04-14 21:21:55 +0300

    cpp02: Started ex00

---
+++ cpp02/ex00/Fixed.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akolupae <akolupae@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/14 21:17:15 by akolupae          #+#    #+#             */
+/*   Updated: 2026/04/14 21:20:05 by akolupae         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class	Fixed {
+
+public:
+
+	Fixed(void);
+	~Fixed(void);
+
+private:
+
+};
+
+#endif
---
+++ cpp02/ex00/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akolupae <akolupae@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/13 18:18:55 by akolupae          #+#    #+#             */
+/*   Updated: 2026/04/14 21:16:41 by akolupae         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int	main(void) {
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
+}
